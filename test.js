42281acc327e79c52e875536a8f33aab1e410db1
quim <joaquim.serafim@gmail.com>
2015-04-21 00:17:05 +0100

    add a new test adding a new key to an object

--- test.js
+++ test.js
@@ -3,28 +3,36 @@
 var test        = require('tape')
 var addKeyValue = require('./')
 
-test('add a new key "version" with a value should be able to use directly' +
-' without returning or by return into a new variable', function(assert) {
+test('add a new key "hello" with a value should be able to use the object' +
+' directly or without returning or by return into a new variable',
+function(assert) {
   var json0 = require('./package.json')
-  var r = addKeyValue(json0, 'version', 'test')
-  assert.deepEqual(json0.version, 'test')
-  assert.deepEqual(r.version, 'test')
+  var r = addKeyValue(json0, 'hello', 'world')
+  assert.deepEqual(json0.hello, 'world')
+  assert.deepEqual(r.hello, 'world')
+  assert.end()
+})
+
+test('override the key "version" with a new value', function(assert) {
+  var json1 = require('./package.json')
+  addKeyValue(json1, 'version', 'test')
+  assert.deepEqual(json1.version, 'test')
   assert.end()
 })
 
 test('don\'t pass a valid key (string) should return "undefined" object',
 function(assert) {
-  var json1 = require('./package.json')
-  var r = addKeyValue(json1, null, 'test')
+  var json2 = require('./package.json')
+  var r = addKeyValue(json2, null, 'test')
   assert.deepEqual(r, undefined)
   assert.end()
 })
 
-test('add a new key "version" with a null/undefined/empty value the key' +
+test('override the key "version" with a null/undefined/empty value the key' +
 ' should have an "undefined" value', function(assert) {
-  var json2 = require('./package.json')
-  addKeyValue(json2, 'version')
-  assert.deepEqual(json2.version, undefined)
+  var json3 = require('./package.json')
+  addKeyValue(json3, 'version')
+  assert.deepEqual(json3.version, undefined)
   assert.end()
 })
 
