2c782cc12796b6a8c25f0c5373f943c51d3698f9
quim <joaquim.serafim@gmail.com>
2015-04-24 09:46:20 +0100

    add a new test

--- test.js
+++ test.js
@@ -6,33 +6,45 @@ var addKeyValue = require('./')
 test('add a new key "hello" with a value should be able to use the object' +
 ' directly or without returning or by return into a new variable',
 function(assert) {
-  var json0 = require('./package.json')
-  var r = addKeyValue(json0, 'hello', 'world')
-  assert.deepEqual(json0.hello, 'world')
+  var pkg = require('./package.json')
+  var r = addKeyValue(pkg, 'hello', 'world')
+  assert.deepEqual(pkg.hello, 'world')
   assert.deepEqual(r.hello, 'world')
   assert.end()
 })
 
+test('pass as a function argument', function(assert) {
+  var obj = {}
+  function fnAdd (o) {
+    assert.equal(o.constructor.name, 'Object')
+    assert.deepEqual(Object.keys(o).length, 1)
+    assert.deepEqual(o.value, 'hello world')
+  }
+  fnAdd(addKeyValue(obj, 'value', 'hello world'))
+  assert.deepEqual(obj.value, 'hello world')
+  assert.end()
+})
+
 test('override the key "version" with a new value', function(assert) {
-  var json1 = require('./package.json')
-  addKeyValue(json1, 'version', 'test')
-  assert.deepEqual(json1.version, 'test')
+  var pkg = require('./package.json')
+  addKeyValue(pkg, 'version', 'test')
+  assert.deepEqual(pkg.version, 'test')
   assert.end()
 })
 
 test('pass an invalid key (string) should return "undefined" object',
 function(assert) {
-  var json2 = require('./package.json')
-  var r = addKeyValue(json2, null, 'test')
+  var pkg = require('./package.json')
+  var r = addKeyValue(pkg, null, 'test')
   assert.deepEqual(r, undefined)
   assert.end()
 })
 
 test('override the key "version" with a null/undefined/empty value the key' +
 ' should have an "undefined" value', function(assert) {
-  var json3 = require('./package.json')
-  addKeyValue(json3, 'version')
-  assert.deepEqual(json3.version, undefined)
+  var pkg = require('./package.json')
+  addKeyValue(pkg, 'version')
+  assert.deepEqual(pkg.version, undefined)
   assert.end()
 })
 
@@ -55,6 +67,7 @@ test('passing a "undefined" value and using the "strict" method should' +
   var nObj = {a: 1, b: 2}
   addKeyValue.strict(nObj, 'c')
   assert.deepEqual(nObj.c, undefined)
+  assert.deepEqual(nObj.a, 1)
   assert.end()
 })
 
